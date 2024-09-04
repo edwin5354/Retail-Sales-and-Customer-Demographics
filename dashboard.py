b0620fd524a3f7d7baa43d72a511cb3c419922b8
Edwin Lam <165879372+edwin5354@users.noreply.github.com>
2024-09-04 19:08:21 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,11 +4,18 @@ import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
 import warnings 
+from prophet import Prophet
+import matplotlib.pyplot as plt
 
 warnings.filterwarnings('ignore')
 
-file_path = "./retail_sales_v2.csv"
+file_path = r"C:\Users\Edwin\Python\bootcamp\Projects\retails\dataset\retail_sales_v2.csv"
 df = pd.read_csv(file_path)
+
+st.title('Retail Sales & Customer Demographics Project')
+st.write("Welcome to the Retail Sales Dashboard! Here, you'll find engaging exploratory data analysis (EDA) visualizations that generate interactive graphs. This project aims to uncover sales trends across different time periods, and we'll also explore techniques for clustering and forecasting analysis. Enjoy diving into the data!")
+
+st.subheader('a) Exploratory Data Analysis')
 
 def interactive_widget(x, y):
     if x == 'Gender':
@@ -23,6 +30,8 @@ def interactive_widget(x, y):
         elif y == 'Purchased Quantity':
             select_df = df.groupby([x, 'Quantity']).size().reset_index(name='Frequency')
             select_df.columns = [x, 'Quantity', 'Frequency']
+        elif y == 'Amount spent per Transaction':
+            select_df = df[['Gender', 'Total Amount']]
     elif x == 'Age Group':
         if y == 'Distribution':
             select_df = df.groupby('Age Group').size().reset_index(name='Distribution')
@@ -35,46 +44,66 @@ def interactive_widget(x, y):
         elif y == 'Purchased Quantity':
             select_df = df.groupby(['Age Group', 'Quantity']).size().reset_index(name='Frequency')
             select_df.columns = ['Age Group', 'Quantity', 'Frequency']
+        elif y == 'Amount spent per Transaction':
+            select_df = df[['Age Group', 'Total Amount']]
 
     return select_df
 
+def plot_data(x, y, show_df):
+    if y == 'Sales by different Categories':
+        fig = px.bar(show_df, x='Product Category', y='Total Sales', color=x,
+                     title=f'Total Sales by Product Category and {x}', barmode='group')
+    elif y == 'Distribution':
+        fig = px.pie(show_df, values='Distribution', names=x, title=f'{x} Distribution')
+    elif y == 'Overall Sales':
+        fig = px.bar(show_df, x=x, y='Total Amount', title=f'Overall Sales by {x}')
+    elif y == 'Purchased Quantity':
+        fig = px.bar(show_df, x='Quantity', y='Frequency', color=x,
+                     title='Purchase Frequency', barmode='group')
+    elif y == 'Amount spent per Transaction':
+        fig = px.box(show_df, x=x_input, y='Total Amount', 
+                     title= f'Boxplot of Amount Spent per Transaction by {x_input}', 
+                     labels={x_input: x_input, 'Total Amount': 'Amount Spent ($)'})
+    
+    st.plotly_chart(fig)
+
+def time_series_widget(x):
+    if x =='Gender':
+        new_df = df.groupby(['Gender', 'Date'])['Total Amount'].sum().reset_index()
+        fig = px.line(new_df, x='Date', y="Total Amount", color= 'Gender',
+                      title = "Time Series Analysis of Total Sales by Gender",
+                      labels={x_input: x_input, 'Total Amount': 'Sales ($)'})
+    elif x == 'Product Category':
+        new_df = df.groupby(['Product Category', 'Date'])['Total Amount'].sum().reset_index()
+        fig = px.line(new_df, x='Date', y="Total Amount", color= 'Product Category',
+                      title = "Time Series Analysis of Total Sales by Product category",
+                      labels={x_input: x_input, 'Total Amount': 'Sales ($)'})
+    elif x == 'Overall Sales':
+        new_df = df.groupby('Date')['Total Amount'].sum().reset_index()
+        fig = px.line(new_df, x='Date', y="Total Amount",
+                      title = "Time Series Analysis of Overall Sales",
+                      labels={x_input: x_input, 'Total Amount': 'Sales ($)'})
+
+    st.plotly_chart(fig)
+
 # Streamlit UI
-x_input = st.selectbox('Choose an input variable:', ['Gender', 'Age'])
-y_input = st.selectbox('Choose an output variable:', ['Purchased Quantity', 'Sales by different Categories', 'Distribution', 'Overall Sales'])
+x_input = st.selectbox('Choose an input variable:', ['Gender', 'Age Group'])
+y_input = st.selectbox('Choose an output variable:', ['Purchased Quantity', 'Sales by different Categories',
+                                                       'Distribution', 'Overall Sales', 'Amount spent per Transaction'])
 
 if x_input == 'Gender':
-    if y_input == 'Sales by different Categories':
-        show_df = interactive_widget('Gender', 'Sales by different Categories')
-        fig = px.bar(show_df, x='Product Category', y='Total Sales', color='Gender',
-                     title='Total Sales by Product Category and Gender', barmode='group')
-        st.plotly_chart(fig)
-    elif y_input == 'Distribution':
-        show_df = interactive_widget('Gender', 'Distribution')
-        fig = px.pie(show_df, values='Distribution', names='Gender', title='Gender Distribution')
-        st.plotly_chart(fig)
-    elif y_input == 'Overall Sales':
-        show_df = interactive_widget('Gender', 'Overall Sales')
-        fig = px.bar(show_df, x='Gender', y='Total Amount', title='Overall Sales by Gender')
-        st.plotly_chart(fig)
-    elif y_input == 'Purchased Quantity':
-        show_df = interactive_widget('Gender', 'Purchased Quantity')
-        fig = px.bar(show_df, x='Quantity', y='Frequency', color='Gender',
-                     title='Purchase Frequency', barmode='group')
-elif x_input == 'Age':
-    if y_input == 'Sales by different Categories':
-        show_df = interactive_widget('Age Group', 'Sales by different Categories')
-        fig = px.bar(show_df, x='Product Category', y='Total Sales', color='Age Group',
-                title='Total Sales by Product Category and Gender', barmode='group')
-        st.plotly_chart(fig)
-    elif y_input == 'Distribution':
-        show_df = interactive_widget('Age Group', 'Distribution')
-        fig = px.pie(show_df, values='Distribution', names='Age Group', title='Age Distribution')
-        st.plotly_chart(fig)
-    elif y_input == 'Overall Sales':
-        show_df = interactive_widget('Age Group', 'Overall Sales')
-        fig = px.bar(show_df, x='Age Group', y='Total Amount', title='Overall Sales by Age Group')
-        st.plotly_chart(fig)
-    elif y_input == 'Purchased Quantity':
-        show_df = interactive_widget('Age Group', 'Purchased Quantity')
-        fig = px.bar(show_df, x='Quantity', y='Frequency', color='Age Group',
-                     title='Purchase Frequency', barmode='group') 
+    show_df = interactive_widget('Gender', y_input)
+elif x_input == 'Age Group':
+    show_df = interactive_widget('Age Group', y_input)
+
+# Call the plotting function
+if not show_df.empty:
+    plot_data(x_input, y_input, show_df)
+else:
+    st.write("No data available for the selected criteria.")
+
+st.subheader('b) Time Series Analysis and Forecasting')
+time_input = st.selectbox('Please select a variable to display the time-series analysis:', ['Overall Sales', 'Gender', 'Product Category'])
+
+if time_input:
+    time_series_widget(time_input)
