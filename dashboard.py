cbcd71fb1070ef21f2dbc9c7d272de8a2ce1d353
Edwin Lam <165879372+edwin5354@users.noreply.github.com>
2024-09-05 15:12:26 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,6 @@ import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
 import warnings 
-from prophet import Prophet
 import matplotlib.pyplot as plt
 
 warnings.filterwarnings('ignore')
